d4ef6908366cc6acc487002c4adf884db4c392fc
urielsarrazin <uriel.sarrazin@digixall.io>
2019-11-26 21:34:00 +0100

    Bootstrapping basic Quarkus project

---
+++ gretting-api/src/test/java/io/digixall/quarkus/gretting/api/GrettingResourceTest.java
@@ -0,0 +1,21 @@
+package io.digixall.quarkus.gretting.api;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class GrettingResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/greeting")
+          .then()
+             .statusCode(200)
+             .body(is("hello"));
+    }
+
+}
---
+++ gretting-api/src/test/java/io/digixall/quarkus/gretting/api/NativeGrettingResourceIT.java
@@ -0,0 +1,9 @@
+package io.digixall.quarkus.gretting.api;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGrettingResourceIT extends GrettingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
